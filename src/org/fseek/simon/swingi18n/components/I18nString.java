1fc72347bb07d43ae8cc503468314e07458f6ca9
Simon Wimmesberger <wimmesberger@gmail.com>
2014-07-08 20:44:32 +0200

    Added a simple utility to get translations on runtime when needed

---
+++ src/org/fseek/simon/swingi18n/components/I18nString.java
@@ -0,0 +1,20 @@
+
+package org.fseek.simon.swingi18n.components;
+
+import org.fseek.simon.swingi18n.I18nController;
+
+/**
+ *
+ * @author Simon Wimmesberger
+ */
+public class I18nString{
+    private final String key;
+    public I18nString(String key){
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return I18nController.getText(this.key);
+    }
+}
